9a194a9b65168de92268393cacdedace3149a5a1
Yasen <yasen9977@gmail.com>
2018-05-03 12:46:22 +0300

    Added punishPlayer function in Administrator class

--- src/users/Administrator.java
+++ src/users/Administrator.java
@@ -1,16 +1,23 @@
 package users;
 
+import java.util.ArrayList;
+
 public class Administrator extends User {
 
 	public Administrator(String userName) {
 		super(userName);
 	}
 	
-	public void punishPlayer(String playerName, int pointsToDeduct) {
-		// find player in ranklist and decrease points
-	}
-	
-	public void addQuestion() {
-		
+	public void punishPlayer(String playerName, int pointsToDeduct, ArrayList<Player> players) {
+		boolean operationDone = false;
+		for (Player p : players) {
+			if (p.getUserName().equals(playerName)) {
+				p.decreasePoints(pointsToDeduct);
+				operationDone = true;
+			}
+		}
+		if (!operationDone) {
+			System.out.println("There is no such player");
+		}
 	}
 }
