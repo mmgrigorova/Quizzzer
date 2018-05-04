665a95985e494fa10d31587a5f823996adecdaeb
Yasen <yasen9977@gmail.com>
2018-05-04 22:30:03 +0300

    added questions and txt file script with questions

--- src/StartQuizzzer.java
+++ src/StartQuizzzer.java
@@ -38,7 +38,7 @@ public class StartQuizzzer {
 // 
 //      f = new FileOutputStream("Questions.txt");
 //      o = new ObjectOutputStream(f); 
-//     
+//     sports questions
 //      Question q = new ClosedQuestion("Which country won the 2014 FIFA World Cup?",QuestionCategory.SPORTS,
 //                                      "Brazil","Germany","Italy","Spain", 2);
 //      Question q1 = new ClosedQuestion("Which country won the first ever soccer World Cup in 1930?",QuestionCategory.SPORTS,
@@ -71,6 +71,44 @@ public class StartQuizzzer {
 //      Question q18 = new OpenQuestion("What is a perfect score in bowling?", 300, QuestionCategory.SPORTS);
 //      Question q19 = new OpenQuestion("How many times has Brazil won the FIFA World Cup?", 5, QuestionCategory.SPORTS);
 //     
+//      
+//      // history questions
+//      
+//      Question q20 = new ClosedQuestion("Who was president of the United States when the nuclear bombs were droped over Hyroshima and Nagasaki?",QuestionCategory.HIST,
+//              "Franklin D Roosewelt","Gerald Ford","Richard Nixon","Harry S Truman", 4);
+//      Question q21 = new ClosedQuestion("Which nation is a direct descendant of the Roman Empire?",QuestionCategory.HIST,
+//              "Italy","Germany","Greece","Zimbabwe", 1);
+//      Question q22 = new ClosedQuestion("What is the main reason as to why upon the Holy Lands there was no and the is no peace to this day?",QuestionCategory.HIST,
+//              "Politics","Religion","Natural resources","Corruption", 1);
+//      Question q23 = new ClosedQuestion("Whose side was Bulgaria on during most of the WW2?",QuestionCategory.HIST,
+//              "Russia","Germany","USA","Uganda", 2);
+//      Question q24 = new ClosedQuestion("Liberty, equality, fraternity\" was the moto of the:",QuestionCategory.HIST,
+//              "American revolution","French Revolution","Iranian Revolution","Russian revolution", 2);
+//      Question q25 = new ClosedQuestion("Whose words were these : \"History is a bunch of lies, agreed upon\"?",QuestionCategory.HIST,
+//              "Emperor Neron","Stalin","JFK","Napoleon", 4);
+//      Question q26 = new ClosedQuestion("What language is the Bible written in?",QuestionCategory.HIST,
+//              "Arameic(Antient arabic) and Greek","Greek and Latin","Chinese","Latin and Greek", 1);
+//      Question q27 = new ClosedQuestion("What ethnicity was Aleksander The Great(Makedonski)?",QuestionCategory.HIST,
+//              "Greek","Bulgarian","Turk","Viking", 1);
+//      Question q28 = new ClosedQuestion("Who invented the concrete?",QuestionCategory.HIST,
+//              "The Vikings","The Romans","The Arabs","Japanese", 2);
+//      Question q29 = new ClosedQuestion("Which was the only country in the world to save its citizen of jewish descend from Hitler's gas chambers?",QuestionCategory.HIST,
+//              "Hungary","Poland","France","Bulgaria", 4);
+//      
+//      Question q30 = new OpenQuestion("Krum was a bulgarian leader known to have Byzantine Emperor Nikephoros skull made to a glass from which he drank.\r\n" + 
+//      		"What was the year at which his reign over Bulgaria started?", 803, QuestionCategory.HIST);
+//      Question q31 = new OpenQuestion("What was the year when Nazi Germany invaded Poland - the WW2 brake out?", 1939, QuestionCategory.HIST);
+//      Question q32 = new OpenQuestion("What was the year of the Bulgarian Declaration of Independence?", 1908, QuestionCategory.HIST);
+//      Question q33 = new OpenQuestion("Which century was Bulgaria founded?",7, QuestionCategory.HIST);
+//      Question q34 = new OpenQuestion("The computation power of the humandkind when we landed on the moon was less than what it is today in a single phone.\r\n" + 
+//      		"What was the year when USA landed its spaceship with astronauts on the moon?", 1969, QuestionCategory.HIST);
+//      Question q35 = new OpenQuestion("When was the first time a human - the russian Juri Gagarin flew in outer space?", 1961, QuestionCategory.HIST);
+//      Question q36 = new OpenQuestion("The Cuban Missile Crisis was a period of two weeks when the world was at its closest to a global nuclear war?\r\n" + 
+//      		"What was the year of it?", 1962, QuestionCategory.HIST);
+//      Question q37 = new OpenQuestion("United States of America had a fresh start without holding a centuries old grudges like old european states. When was it founded?", 1776, QuestionCategory.HIST);
+//      Question q38 = new OpenQuestion("Pythagoras ws an antient greek philosopher and mathematitian. What century BC was he born?",6, QuestionCategory.HIST);
+//      Question q39 = new OpenQuestion("How many times was the pope a woman?", 1, QuestionCategory.HIST);
+//      
 //      list.add(q);
 //      list.add(q1);
 //      list.add(q2);
@@ -91,7 +129,81 @@ public class StartQuizzzer {
 //      list.add(q17);
 //      list.add(q18);
 //      list.add(q19);
+//      
+//      list.add(q20);
+//      list.add(q21);
+//      list.add(q22);
+//      list.add(q23);
+//      list.add(q24);
+//      list.add(q25);
+//      list.add(q26);
+//      list.add(q27);
+//      list.add(q28);
+//      list.add(q29);
+//      list.add(q30);
+//      list.add(q31);
+//      list.add(q32);
+//      list.add(q33);
+//      list.add(q34);
+//      list.add(q35);
+//      list.add(q36);
+//      list.add(q37);
+//      list.add(q38);
+//      list.add(q39);
+//      
+//      Question q40 = new ClosedQuestion("Which is the capital of Australia?",QuestionCategory.GEO,
+//              "Sofia","Sidney","Canberra","Ottawa", 3);
+//      Question q41 = new ClosedQuestion("Where do polar bears reside?",QuestionCategory.GEO,
+//              "Arctic region plus some northern countries ","South Pole along with the penguins","Siberia","South Africa", 1);
+//      Question q42 = new ClosedQuestion("Which is the highest waterfall in the world?",QuestionCategory.GEO,
+//              "Niagara Falls(USA/Canad)","Angel Falls(Venezuela)","Victoria Falls(Zambia/Zimbabwe)","Raisko praskalo(Bulgaria)", 2);
+//      Question q43 = new ClosedQuestion("Which is/are the first countrie/s to celebrate the New Year?",QuestionCategory.GEO,
+//              "Samoa, Tonga, Kiribati","Japan","Chile, Easter Islands","New Zealand", 1);
+//      Question q44 = new ClosedQuestion("Which continent has the most countries?",QuestionCategory.GEO,
+//              "Asia","Africa","Europe","South America", 2);
+//      Question q45 = new ClosedQuestion("Which is the biggest country in the world?",QuestionCategory.GEO,
+//              "Canada","China","USA","Russia", 4);
+//      Question q46 = new ClosedQuestion("Which is the largest freshwater lake in the world?",QuestionCategory.GEO,
+//              "Caspian sea","Pancharevo","Lake Baikal","Lake Victoria", 3);
+//      Question q47 = new ClosedQuestion("Where is Dead Sea?",QuestionCategory.GEO,
+//              "Between Jordan, Israel and Palestine","Between Uganda and Tanzania","Between Saudi Arabia and Yemen","Namibia", 1);
+//      Question q48 = new ClosedQuestion("Which is the largest ocean in the world?",QuestionCategory.GEO,
+//              "Indian ocean","Pacific ocean","Atlantic ocean","Arctic ocean", 2);
+//      Question q49 = new ClosedQuestion("Which is the capital of Brazil?",QuestionCategory.GEO,
+//              "Sao Paulo","Rio de Janeiro","Brasilia","Bucharest", 3);
+//      
+//      Question q50 = new OpenQuestion("How many countries are there in the European Union?", 28, QuestionCategory.GEO);
+//      Question q51 = new OpenQuestion("What is the highest temperature ever measured(in celsius)?", 58, QuestionCategory.GEO);
+//      Question q52 = new OpenQuestion("What is the absolute zero(0 Kelvin scale) at which no thermal energy is present and you can't go lower/colder than that?", 273, QuestionCategory.GEO);
+//      Question q53 = new OpenQuestion("What is the altitude of peak Everest in the Himalayas in meters?", 8848, QuestionCategory.GEO);
+//      Question q54 = new OpenQuestion("What is the altitude of Bulgaria's highest peak Musala in  meters?", 2925, QuestionCategory.GEO);
+//      Question q55 = new OpenQuestion("How many years ago(in millions) did the dinosaurs perish?",66, QuestionCategory.GEO);
+//      Question q56 = new OpenQuestion("How many Great Lakes are there(North America)?", 5, QuestionCategory.GEO);
+//      Question q57 = new OpenQuestion("The United Kingdom is comprised of how many countries", 4, QuestionCategory.GEO);
+//      Question q58 = new OpenQuestion("What is the approximate size(in thousands) of Earth's equator?", 40, QuestionCategory.GEO);
+//      Question q59 = new OpenQuestion("What is the approximate population of Germany(in millions)?", 82, QuestionCategory.GEO);
 //     
+//      list.add(q40);
+//      list.add(q41);
+//      list.add(q42);
+//      list.add(q43);
+//      list.add(q44);
+//      list.add(q45);
+//      list.add(q46);
+//      list.add(q47);
+//      list.add(q48);
+//      list.add(q49);
+//      list.add(q50);
+//      list.add(q51);
+//      list.add(q52);
+//      list.add(q53);
+//      list.add(q54);
+//      list.add(q55);
+//      list.add(q56);
+//      list.add(q57);
+//      list.add(q58);
+//      list.add(q59);
+//      
 //      for (Question quest : list) {
 //          o.writeObject(quest);
 //      }
